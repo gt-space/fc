50c07eca0a8ab73e03867ff63540a3f472ccb14b
Aras Shirwan <arascshirwan@gmail.com>
2025-01-17 11:56:28 -0500

    Basic Testing

---
+++ src/servo.rs
@@ -0,0 +1,26 @@
+use std::net::{TcpStream, ToSocketAddrs};
+use common::{comm::VehicleState, sequence::Duration};
+use crate::SERVO_ADDRESS;
+
+/// Establishes a TCP stream with Servo.
+// TODO: Implement a stop such that after a certain number of tries it gives up and aborts
+pub(super) fn establish() -> TcpStream {
+
+  println!("Attempting connection with servo at {}:{}...", SERVO_ADDRESS.0, SERVO_ADDRESS.1);
+  loop {
+    match TcpStream::connect(SERVO_ADDRESS) {
+        Ok(s) => return s,
+        Err(e) => eprintln!("Connection couldn't be established due to: {e}. Retrying..."),
+    };
+  };
+}
+
+/// Gets new config/mapping information from Servo
+pub(super) fn pull(stream: &mut TcpStream) {
+  panic!("servo::pull: Not Implemented!");
+}
+
+/// Sends new VehicleState to servo
+pub(super) fn push(stream: &mut TcpStream, state: VehicleState) {
+  panic!("servo::push: Not Implemented!");
+}
---
+++ src/state.rs
@@ -0,0 +1,9 @@
+use common::comm::VehicleState;
+
+struct Data;
+// TODO: Maybe we could make this a method of VehicleState for 
+//       the common refactor?
+/// Updates the vehicle state with the new data recieved from devices
+pub(super) fn ingest(state: &mut VehicleState, data: Vec<Data>) {
+  panic!("state::ingest: Not Implemented!");
+}
