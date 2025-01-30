a220d10455f3c383d7d551632ac34eda3625cbd0
Aras Shirwan <arascshirwan@gmail.com>
2025-01-29 21:10:54 -0500

    Added functionality for mmap and recieving commands from sequences.

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,30 @@ mod state;
 mod sequence;
 
 use std::{os::unix::net::UnixDatagram, collections::HashMap, net::{TcpListener, TcpStream}, time::Duration, process::Output};
-
 use common::comm::{flight::BoardId, NodeMapping, VehicleState};
+use mmap_sync::synchronizer::Synchronizer;
 
 /// The address that boards can connect to
 const LISTENER_ADDRESS: (&str, u16) = ("0.0.0.0", 4573);
 const SERVO_ADDRESS: (&str, u16) = ("192.168.1.10", 5025);
 const SAM_PORT: u16 = 8378;
 const DATAGRAM_PATH: &str = "";
+const MMAP_PATH: &str = "";
+const MMAP_GRACE_PERIOD: Duration = Duration::from_millis(100);
 
 fn main() -> ! {
     println!("Flight Computer running at version {}\n", env!("CARGO_PKG_VERSION"));
     let listener = TcpListener::bind(LISTENER_ADDRESS)
         .expect(&format!("Couldn't open port {} on IP address {}", LISTENER_ADDRESS.1, LISTENER_ADDRESS.0));
     listener.set_nonblocking(true).expect("Cannot set listener to non-blocking.");
-    let mut devices: HashMap<BoardId, TcpStream> = HashMap::new();
+    let devices: HashMap<BoardId, TcpStream> = HashMap::new();
     let sam_mappings: Vec<NodeMapping> = Vec::new();
     let vehicle_state = VehicleState::new();
     let sequences: HashMap<String, Output> = HashMap::new();
     let commands: UnixDatagram = UnixDatagram::bind(DATAGRAM_PATH)
         .expect(&format!("Could not open sequence command socket on path '{DATAGRAM_PATH}'."));
+    commands.set_nonblocking(true).expect("Cannot set sequence command socket to non-blocking.");
+    let synchronizer: Synchronizer = Synchronizer::new(MMAP_PATH.as_ref());
 
     let mut servo_stream = servo::establish(SERVO_ADDRESS, 3, Duration::from_secs(2)).expect("Could't set up initial servo connection");
     loop {
--- src/sequence.rs
+++ src/sequence.rs
@@ -1,5 +1,7 @@
-use common::comm::{NodeMapping, SensorType, Sequence};
-use std::{collections::HashMap, io, process::{Child, Command}};
+use common::comm::{sam::SamControlMessage, CompositeValveState, NodeMapping, SensorType, Sequence, ValveState, VehicleState};
+use std::{collections::HashMap, io, os::unix::net::UnixDatagram, process::{Child, Command}};
+
+// TODO: Refactor this code to use custom error types
 
 fn run(mappings: &Vec<NodeMapping>, sequence: &Sequence) -> io::Result<Child> {
     let mut script = String::from("from sequences import *;");
@@ -13,7 +15,9 @@ fn run(mappings: &Vec<NodeMapping>, sequence: &Sequence) -> io::Result<Child> {
     }
     
     script.push_str(&sequence.script);
-    Command::new("python3").args(["-c", &script]).spawn()
+    Command::new("python3")
+        .args(["-c", &script])
+        .spawn()
 }
 
 pub(crate) fn execute(mappings: &Vec<NodeMapping>, sequence: Sequence, sequences: &mut HashMap<String, Child>) {
@@ -42,10 +46,10 @@ pub(crate) fn execute(mappings: &Vec<NodeMapping>, sequence: Sequence, sequences
     sequences.insert(sequence.name, process);
 }
 
-pub(crate) fn abort(sequences: &mut HashMap<String, Child>, name: &String) -> io::Result<()> {
+pub(crate) fn kill(sequences: &mut HashMap<String, Child>, name: &String) -> io::Result<()> {
     let sequence = match sequences.get_mut(name) {
         Some(c) => {
-            if let Ok(None) = c.try_wait() {
+            if let Ok(Some(_)) = c.try_wait() {
                 println!("A sequence named '{name}' isn't running.");
                 return Ok(());
             }
@@ -59,4 +63,70 @@ pub(crate) fn abort(sequences: &mut HashMap<String, Child>, name: &String) -> io
     };
 
     sequence.kill()
+}
+
+// TODO: Put this in common?
+#[derive(serde::Deserialize)]
+enum SamCommand {
+    ActuateValve { valve: String, state: ValveState },
+    Abort,
+}
+
+pub(crate) fn handle_commands(socket: &UnixDatagram, mappings: &Vec<NodeMapping>, vehicle_state: &mut VehicleState) -> Vec<SamControlMessage> {
+    let mut buf: [u8; 1024] = [0; 1024];
+    let mut commands: Vec<SamControlMessage> = Vec::new();
+
+    loop {
+        let size = match socket.recv(&mut buf) {
+            Ok(s) => s,
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => break,
+            Err(e) => {
+                eprintln!("Error in recieving from sequence command socket: {e}");
+                break;
+            }
+        };
+
+        let command = match postcard::from_bytes::<SamCommand>(&buf[..size]) {
+            Ok(c) => c,
+            Err(e) => {
+                eprintln!("Error in deserializing SamCommand from sequence: {e}");
+                continue;
+            }
+        };
+
+        match command {
+            SamCommand::ActuateValve { valve, state } => {
+                let Some(mapping) = mappings.iter().find(|m| m.text_id == valve) else {
+                    eprintln!("Failed to actuate valve: mapping '{valve}' is not defined.");
+                    continue;
+                };
+
+                let closed = state == ValveState::Closed;
+                let normally_closed = mapping.normally_closed.unwrap_or(true);
+                let powered = closed != normally_closed;
+
+                commands.push(
+                    SamControlMessage::ActuateValve {
+                        channel: mapping.channel,
+                        powered,
+                    }
+                );
+
+                if let Some(existing) = vehicle_state.valve_states.get_mut(&valve) {
+                    existing.commanded = state;
+                } else {
+                    vehicle_state.valve_states.insert(
+                        valve,
+                        CompositeValveState {
+                            commanded: state,
+                            actual: ValveState::Undetermined
+                        }
+                    );
+                }
+            }
+            SamCommand::Abort => todo!()
+        }
+    }
+
+    commands
 }
