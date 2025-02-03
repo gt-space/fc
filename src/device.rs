29da84c37b8091754e5fc9143da6bef89d29ebe2
sgarg330 <sgarg330@gatech.edu>
2025-02-03 04:45:16 -0500

    pull and ingest methods layout

--- src/device.rs
+++ src/device.rs
@@ -1,7 +1,11 @@
-use std::{io, net::{TcpListener, TcpStream}};
+use std::{io, net::{TcpListener, TcpStream, UdpSocket}, time::Duration, collections::HashSet};
 use crate::config::{ip_to_id, BoardId};
-use common::comm::{flight::DataMessage, sam::SamControlMessage};
+use common::comm::{flight::{DataMessage}, sam::SamControlMessage};
+use std::io::Read;
 
+
+
+//No longer needed for Udp?
 pub(crate) fn listen(listener: &TcpListener) -> Vec<(BoardId, TcpStream)> {
     let mut connections = Vec::new();
     
@@ -29,8 +33,71 @@ pub(crate) fn listen(listener: &TcpListener) -> Vec<(BoardId, TcpStream)> {
 
 // lifetime specifiers here are tricky. we want the data within the data message to be dropped
 // once it's processed by state::ingest
-pub(crate) fn pull<'a, 'b>(devices: impl Iterator<Item = &'a mut TcpStream>) -> Vec<DataMessage<'b>> {
-    todo!()
+pub(crate) fn pull<'b>(socket: &UdpSocket) -> Vec<DataMessage<'b>> {
+
+    let mut collected_data = Vec::new();
+    const BUFFER_SIZE: usize = 1024;
+    let mut buffer = [0u8; BUFFER_SIZE];
+    socket.set_nonblocking(true).expect("Could not set non-blocking"); 
+    socket.set_read_timeout(Some(Duration::from_millis(100))).unwrap();
+    
+    //this should be part of shared maybe or some sort of global property?
+    //let mut boards: HashSet<BoardId> = HashSet::new();
+
+    loop{
+        match socket.recv_from(&mut buffer) {
+            Ok((n, senderAddr)) => {
+                if(n==0) {
+                    continue; 
+                    //no data
+                }
+              let id_result = ip_to_id(senderAddr.ip());  
+              match id_result {
+                Ok(id) => {
+                    let incoming_data = postcard::from_bytes(&buffer[..n]);
+                    let incoming_data = match incoming_data {
+                      Ok(data) => data,
+                      Err(error) => {
+                        eprintln!("Failed to interpret buffer data: {error}");
+                        continue;
+                      }
+                    };
+                    let message = match incoming_data {
+                        DataMessage::Identity(board_id) => {
+                            collected_data.push(DataMessage::Identity(id.to_string()));
+                            //needs to send an identity message back to sender 
+                        }
+                        DataMessage::Sam(board_id, datapoints) => {
+                            collected_data.push(DataMessage::Sam((id.to_string()), (datapoints)));
+                        }
+
+                        DataMessage::Bms(board_id, datapoints) => {
+                            collected_data.push(DataMessage::Bms((id.to_string()), (datapoints)));
+                        }
+                        DataMessage::Ahrs(board_id, datapoints) => {
+                            collected_data.push(DataMessage::Ahrs((id.to_string()), (datapoints)));
+                        }
+                        DataMessage::FlightHeartbeat => {
+                            //not a message that we will receive
+                        }
+                    }; 
+                }
+                Err(err) => {
+                    eprintln!("Could not convert ip to id");
+                }
+              }
+            }
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                // No data 
+                //Maybe we Check heartbeats
+                break;
+            }
+            Err(e) => {
+                eprintln!("Error with parsing UDP data: {} " , e );
+            }
+        }
+    }
+    collected_data
 }
 
 // create a function or series of functions that takes a command and sends it to a board
--- src/state.rs
+++ src/state.rs
@@ -1,6 +1,61 @@
-use common::comm::{flight::DataMessage, VehicleState};
+use common::comm::{ahrs::DataPoint, flight::DataMessage, VehicleState};
+use common::comm::ahrs;
+use crate::config::{BoardId};
+
 
 /// Updates the vehicle state with the new data recieved (flight 1.0 code can be reused)
+trait DataHandler{
+  fn update_state(&self, state: &mut VehicleState, data: DataMessage);
+}
+
+struct SamHandler;
+struct BmsHandler;
+struct AhrsHandler;
+
+impl DataHandler for SamHandler {
+  fn update_state(&self, state: &mut VehicleState, data: DataMessage) {
+      //need help here 
+  }
+}
+
+impl DataHandler for BmsHandler {
+  fn update_state(&self, state: &mut VehicleState, data: DataMessage) {
+    if let DataMessage::Bms(board_id, datapoint) = data {
+        state.bms = datapoint.state;  //based on exisitng code
+      
+    }
+  }
+}
+
+impl DataHandler for AhrsHandler {
+  fn update_state(&self, state: &mut VehicleState, data: DataMessage) {
+    if let DataMessage::Ahrs(board_id, datapoints) = data {
+      let datapoints_slice: &[ahrs::DataPoint]  = &datapoints;
+      for datapoint in datapoints_slice {
+        state.ahrs = datapoint.state;  //based on exisitng code
+      }
+    }
+  }
+}
+
 pub(super) fn ingest(state: &mut VehicleState, data: Vec<DataMessage>) {
-  todo!()
+  for message in data {
+    match &message {
+      DataMessage::Ahrs(id, datapoints) => {
+        AhrsHandler.update_state(state, message);
+      }
+      DataMessage::Bms(id, datapoint) => {
+        BmsHandler.update_state(state, message);
+      }
+      DataMessage::Sam(id, datapoints) => {
+        SamHandler.update_state(state, message);
+      }
+      DataMessage::Identity(id) => {
+        //add it to the mappings?
+      }
+      DataMessage::FlightHeartbeat => {
+        //handle heartbeats as needed
+      }
+    };
+  }
 }
